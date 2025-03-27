97960f2a81af3adcc6b3e87bd278486ce73b21d7
Ayushi Paladiya <ayushi.paladiya@shunyavkash.com>
2025-03-27 18:16:42 +0530

    commit

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,14 +1,44 @@
-const openReportsBtn = document.getElementById('openModals');
-const reportModal = document.getElementById('report-modals'); 
-const closeReportsBtn = reportModal?.querySelector('.close-icon'); 
+document.addEventListener("DOMContentLoaded", function () {
+    const openReportsBtn = document.getElementById("openModals");
+    const reportModal = document.getElementById("report-modals");
+    const closeReportsBtn = document.querySelector(".close-icon");
+    const body = document.body;
 
-if (openReportsBtn && reportModal && closeReportsBtn) {
-    openReportsBtn.addEventListener('click', function() {
-        reportModal.style.display = 'block';
-    });
-    closeReportsBtn.addEventListener('click', function() {
-        reportModal.style.display = 'none';
-    });
-}
+    if (openReportsBtn && reportModal && closeReportsBtn) {
+        openReportsBtn.addEventListener("click", function () {
+            reportModal.classList.add("show");
+            body.style.overflow = "hidden";
+        });
 
+        closeReportsBtn.addEventListener("click", function () {
+            reportModal.classList.remove("show");
+            body.style.overflow = "auto";
+        });
+
+        reportModal.addEventListener("click", function (e) {
+            if (e.target === reportModal) {
+                reportModal.classList.remove("show");
+                body.style.overflow = "auto";
+            }
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+       
+    const openModalBtn = document.getElementById('openModal');
+    const myModal = document.getElementById('myModal');
+    const closeModalBtn = myModal?.querySelector('.close'); 
+
+    if (openModalBtn && myModal && closeModalBtn) {
+        openModalBtn.addEventListener('click', function() {
+            myModal.style.display = 'flex';
+            $("body").css('overflow', "hidden");
+        });
+        closeModalBtn.addEventListener('click', function() {
+            myModal.style.display = 'none';
+            $("body").css('overflow', "auto");
+        });
+    }
+});
 
