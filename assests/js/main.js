3fb3d71f6555a1c171ca28e3d4936f19c257b888
Ayushi Paladiya <ayushi.paladiya@shunyavkash.com>
2025-04-03 14:51:18 +0530

    commit

--- assests/js/main.js
+++ assests/js/main.js
@@ -80,33 +80,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-
-const dropdowns = document.querySelectorAll(".select-dropdowns");
-
-dropdowns.forEach((dropdown) => {
-  const select = document.querySelector(".select");
-  const caret = document.querySelector(".caret");
-  const menu = document.querySelector(".menu");
-  const options = document.querySelectorAll(".menu li");
-  const selected = document.querySelector(".selected");
-  select.addEventListener("click", () => {
-    caret.classList.toggle("caret-rotate");
-    menu.classList.toggle("menu-open");
-  });
-
-  options.forEach((option) => {
-    option.addEventListener("click", () => {
-      selected.classList.remove("placeholder");
-      selected.innerText = option.innerText;
-      caret.classList.remove("caret-rotate");
-      menu.classList.remove("menu-open");
-      options.forEach((option) => option.classList.remove("active"));
-      option.classList.add("active");
-    });
-  });
-});
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const openReportsBtn = document.getElementById("open-Modals");
     const reportModal = document.getElementById("cart-modals");
